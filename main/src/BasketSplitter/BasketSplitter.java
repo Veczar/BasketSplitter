0ebde12cc6aa1cda983a8533c981f0c1bbbd143e
Filip Jędrzejek <veczarovsky@gmail.com>
2024-04-06 19:54:15 +0200

    Empty basket splitter

---
+++ main/src/BasketSplitter/BasketSplitter.java
@@ -0,0 +1,14 @@
+package BasketSplitter;
+
+import java.util.List;
+import java.util.Map;
+
+public class BasketSplitter {
+
+    public BasketSplitter(String absolutePathToConfigFile){
+
+    }
+    public Map<String, List<String>> split(List<String> items){
+        return null;
+    }
+}
